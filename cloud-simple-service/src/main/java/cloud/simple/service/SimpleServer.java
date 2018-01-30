c169c3b900740ba922217ef9937bde90d9223ec4
cherryshao <975873553@qq.com>
2018-01-30 16:10:26 +0800

    Initial commit

---
+++ cloud-simple-service/src/main/java/cloud/simple/service/SimpleServer.java
@@ -0,0 +1,15 @@
+package cloud.simple.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
+@RefreshScope
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SimpleServer {
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleServer.class, args);
+    }
+}
