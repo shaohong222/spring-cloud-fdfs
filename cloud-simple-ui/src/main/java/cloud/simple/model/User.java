a0f1b2d4e6853bfba1256c05ba6780dd12eb6914
cherryshao <975873553@qq.com>
2018-01-30 14:56:49 +0800

    Initial commit

---
+++ cloud-simple-ui/src/main/java/cloud/simple/model/User.java
@@ -0,0 +1,14 @@
+package cloud.simple.model;
+
+
+public class User {
+
+	private String username;
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+}
